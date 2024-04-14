aaa9b8a29bcffb68c4a9b0cf5d6f841903bf6ef8
Alya Mansoor <ashin.knox@gmail.com>
2024-04-14 06:08:07 -0500

    Just needing team member bios - finalized all the rest of the about page.

--- RiverHacks24/client/src/pages/About.jsx
+++ RiverHacks24/client/src/pages/About.jsx
@@ -7,28 +7,29 @@ const About = () => {
     
     <div class = "title_div">
       <h1 class = "title_h1">About Girl PowerEd</h1>
-      <br></br>
       <p class = "p_about"> Introducing "Girl PowerEd," where the future of STEM education is fun, 
         inclusive, and powered by women! Our platform is designed to inspire and educate young girls 
-        in the exciting world of science, technology, engineering, and mathematics (STEM). Through a 
-        unique blend of nostalgic call-backs, modern learning techniques, and awe-inspiring female
-        mentors, we're revolutionizing the accessability to STEM for young girls.
+        in the exciting world of science, technology, engineering, and mathematics (STEM). 
+        How, you may ask?
+        <br></br>
+        <br></br>
+        Our approach is simple yet effective: Girl PowerEd is a website where dedicated female mentors, 
+        actively engaged in STEM studies or professions, deliver live and recorded customized workshops 
+        focused on essential STEM skills like coding. Embracing a nostalgic twist, projects are designed 
+        to infuse fun into learning; for example, mastering Python by coding beloved childhood games 
+        like Webkinz. From coding to electronics to 3-D modeling with CAD, our curriculum covers a wide 
+        range of topics, ensuring that girls can explore their interests and discover their passions 
+        with confidence. These mentors offer live teaching sessions and conduct Q&A sessions to maintain 
+        the freshness and engagement of recorded materials, ensuring an enriching experience for young 
+        women exploring STEM.
         <br></br>
         <br></br>
         At Girl PowerEd, we believe that every girl has the potential to excel in STEM, regardless 
-        of background or experience. That's why we've gathered a team of passionate college students, 
-        graduate scholars, and professionals—all women with expertise in various STEM fields—to guide 
-        and mentor the next generation of female innovators. Our approach is simple yet effective: 
-        we leverage the power of familiar childhood games, books, and media to teach complex STEM 
-        concepts in a way that's engaging and relatable. From coding to electronics to 3-D modeling 
-        with CAD, our curriculum covers a wide range of topics, ensuring that girls can explore their 
-        interests and discover their passions with confidence.
-        <br></br>
-        <br></br>
-        With Girl PowerEd, learning STEM isn't just educational—it's an adventure! Whether it's 
-        unraveling the mysteries of coding through beloved classics like Minesweeper or unlocking 
-        the secrets of electronics with interactive challenges inspired by Tamagotchi, our platform 
-        transforms learning into an exciting journey of discovery.
+        of background or experience. That's why Girl PowerEd places a heavy emphases on accessibility,
+        diversity, equity, and inclusivity. Our website has been specifically curated to ensure that 
+        individuals with color blindness or other visual impairments can clearly see and read materials. 
+        We are committed to providing accommodations and welcome feedback from our users on how we can 
+        better serve them!
         <br></br>
         <br></br>
         Our motto, "Who codes? Girls code," embodies our commitment to empowering girls to embrace 
@@ -44,10 +45,77 @@ const About = () => {
     
     <div class = "title_div">
       <h1 class = "title_h1">Why the World Needs Girl PowerEd</h1>
+      <p class = "p_about">Studies have shown that women and men show no significant difference in their 
+        STEM-related abilities; despite this, women are still underrepresented in high-paying STEM jobs.
+        Studies have shown this gap for years. It's surprising, considering how companies led by women 
+        often perform better than those lacking their leadership.
+        <br></br>
+        <br></br>
+        This shows a big challenge: even 
+        though women have the skills and potential for success in STEM, there are still barriers 
+        stopping them from fully participating and advancing in these fields.
+        <br></br>
+        <br></br>
+        Girl PowerEd aims to tackle these barriers, here and now, by empowering young women who are our
+        future leaders of tomorrow.</p>
+        <p class = "p_about"><h3 class = "subtitle_h3">The Numbers Speak for Themselves!</h3>
+        <br></br>
+        What have studies discovered about women's success, and society's success, from girls being in STEM?
+        Let's take a look at some notable statistics about young women in STEM:
+        <br></br>
+        <br></br>
+        <ul>
+          <li><u>ECONOMIC GROWTH:</u> Encouraging more women to pursue STEM careers can significantly 
+            contribute to economic growth and innovation. According to a report by the National 
+            Center for Women & Information Technology (NCWIT), increasing female participation in 
+            the STEM workforce could lead to a <mark>12% growth</mark> in GDP.</li>
+            <br></br>
+            <li><u>DIVERSITY DRIVES INNOVATION:</u> Diversity in the STEM workforce leads to more innovative 
+              solutions and better outcomes. Research from McKinsey & Company found that companies with 
+              gender-diverse executive teams were <mark>21% more likely</mark> to outperform their 
+              competitors in terms of profitability.</li>
+            <br></br>
+            <li><u>ADDRESING GENDER DISPARITIES:</u> Despite progress, gender disparities persist in 
+            STEM fields. Women are underrepresented in STEM occupations, comprising only <mark>28% of 
+            the workforce</mark> according to the U.S. Department of Commerce. Increasing female participation 
+            can help address these disparities and create more equitable opportunities for all.</li>
+            <br></br>
+            <li><u>HERE'S WHY Girl PowerEd's AUDIENCE IS YOUNG WOMEN:</u> According to the "Generation STEM" 
+            report by the Girl Scout Research Institute, <mark>74%</mark> of high school girls across the 
+            country are interested in the fields and subjects of STEM. Perceived gender barriers are still 
+            high for girls and may help explain why STEM fields aren’t their top career choices.</li>
+        </ul>
+        
+        </p>
     </div>
       
     <div class = "title_div">
       <h1 class = "title_h1">Meet the Girl PowerEd Founders</h1>
+
+      <div class="team_member">
+        <center><img src="https://cdn-icons-png.flaticon.com/512/4329/4329449.png" width="150"></img></center>
+        <h3 class = "names_h3">Linh Huynh</h3>
+        <p class="p_bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+      </div>
+      
+      <div class="team_member">
+        <center><img src="https://cdn-icons-png.flaticon.com/512/4329/4329449.png" width="150"></img></center>
+        <h3 class = "names_h3">Alya Mansoor</h3>
+        <p class="p_bio">Aspiring electrical engineer, current mathematician, physicist, and artist, striving to be a part of kind and sustainable solutions.</p>
+      </div>
+      
+      <div class="team_member">
+        <center><img src="https://cdn-icons-png.flaticon.com/512/4329/4329449.png" width="150"></img></center>
+        <h3 class = "names_h3">Avery McClendon</h3>
+        <p class="p_bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+      </div>
+      
+      <div class="team_member">
+        <center><img src="https://cdn-icons-png.flaticon.com/512/4329/4329449.png" width="150"></img></center>
+        <h3 class = "names_h3">Jamie Pham</h3>
+        <p class="p_bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+      </div>
+
     </div>
       
       </>
