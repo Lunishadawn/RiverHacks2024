71c74443f8504a1b5b82607e950dcb37ace550d7
Linh Huynh <133999148+asterphine@users.noreply.github.com>
2024-04-13 19:14:57 -0500

    Nav bar style

--- RiverHacks24/client/src/components/NavBar.jsx
+++ RiverHacks24/client/src/components/NavBar.jsx
@@ -2,39 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import CreateBlog from '../pages/CreateBlog';
-
+import '../css/NavBar.css'
 
 const NavBar = () => {
     
   return (
     <>
 <nav className="navbar navbar-expand bg-body-tertiary">
-  <div className="container-fluid min-vw-100">
-    <a className="navbar-brand" href="#">Navbar</a>
+  <div className="container-fluid min-vw-100" id="container">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-        <Link className="nav-link">Home</Link>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" id='home' href="javascript:void(0)">Home</a>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Home</Link>
+        <li class="nav-item">
+          <a class="nav-link" id='about' href="javascript:void(0)">About</a>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+        <li class="nav-item">
+          <a class="nav-link" id='blogs' href="javascript:void(0)">Blogs</a>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/blogs">Blogs</Link>
+        <li class="nav-item">
+          <a class="nav-link" id='create' href="javascript:void(0)">Create</a>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/create">Create</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/resources">Resources</Link>
+        <li class="nav-item">
+          <a class="nav-link" id='resources' href="javascript:void(0)">Resources</a>
         </li>
       </ul>
+      <form class="d-flex">
+        <button class="btn btn-primary" id='login-btn' type="button">Login</button>
+      </form>
     </div>
   </div>
 </nav>
