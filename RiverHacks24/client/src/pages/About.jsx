687b8b42bf0854ef052b55c215c98908656ed6ea
Alya Mansoor <ashin.knox@gmail.com>
2024-04-13 23:31:17 -0500

    Added titles and paragraphs to the about page to start off, made a rough draft to get a conceptual idea of content on this page.

--- RiverHacks24/client/src/pages/About.jsx
+++ RiverHacks24/client/src/pages/About.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import './about.css'
 
 const About = () => {
   return (
-    <div>Contact</div>
+    <>
+    
+    <div class = "title_div">
+      <h1 class = "title_h1">About Girl PowerEd</h1>
+      <br></br>
+      <p class = "p_about"> Introducing "Girl PowerEd," where the future of STEM education is fun, 
+        inclusive, and powered by women! Our platform is designed to inspire and educate young girls 
+        in the exciting world of science, technology, engineering, and mathematics (STEM). Through a 
+        unique blend of nostalgic call-backs, modern learning techniques, and awe-inspiring female
+        mentors, we're revolutionizing the accessability to STEM for young girls.
+        <br></br>
+        <br></br>
+        At Girl PowerEd, we believe that every girl has the potential to excel in STEM, regardless 
+        of background or experience. That's why we've gathered a team of passionate college students, 
+        graduate scholars, and professionals—all women with expertise in various STEM fields—to guide 
+        and mentor the next generation of female innovators. Our approach is simple yet effective: 
+        we leverage the power of familiar childhood games, books, and media to teach complex STEM 
+        concepts in a way that's engaging and relatable. From coding to electronics to 3-D modeling 
+        with CAD, our curriculum covers a wide range of topics, ensuring that girls can explore their 
+        interests and discover their passions with confidence.
+        <br></br>
+        <br></br>
+        With Girl PowerEd, learning STEM isn't just educational—it's an adventure! Whether it's 
+        unraveling the mysteries of coding through beloved classics like Minesweeper or unlocking 
+        the secrets of electronics with interactive challenges inspired by Tamagotchi, our platform 
+        transforms learning into an exciting journey of discovery.
+        <br></br>
+        <br></br>
+        Our motto, "Who codes? Girls code," embodies our commitment to empowering girls to embrace 
+        their potential as creators, problem-solvers, and leaders in the world of STEM. By providing 
+        access to quality education and mentorship in a supportive and inclusive environment, we're 
+        paving the way for a brighter, more diverse future in STEM. 
+        <br></br>
+        <br></br>
+        Join us at Girl PowerEd and unleash your inner scientist, engineer, and tech whiz! 
+        Together, let's rewrite the narrative of STEM education and inspire a new generation of girls 
+        to dream big, break barriers, and change the world—one line of code at a time.</p>
+    </div>
+    
+    <div class = "title_div">
+      <h1 class = "title_h1">Why the World Needs Girl PowerEd</h1>
+    </div>
+      
+    <div class = "title_div">
+      <h1 class = "title_h1">Meet the Girl PowerEd Founders</h1>
+    </div>
+      
+      </>
   )
 }
 
