f7463a7f2ca2bcbfa299a9c1031734ef08c2f5b4
Linh Huynh <133999148+asterphine@users.noreply.github.com>
2024-04-14 01:41:22 -0500

    created and styled blog page, dropdown, and card

---
+++ RiverHacks24/client/src/components/Dropdown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Dropdown.css'
+
+const Dropdown = () => {
+    return (    
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" id='btn' type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Experience
+            </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Link 1</a></li>
+            <li><a class="dropdown-item" href="#">Link 2</a></li>
+            <li><a class="dropdown-item" href="#">Link 3</a></li>
+        </ul>
+        </div>
+    )
+}
+
+export default Dropdown;
